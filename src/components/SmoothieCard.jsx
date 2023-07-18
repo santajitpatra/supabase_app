bdc9a4e625eb25c814d96258e21a024d5cb7e17b
Santajit Patra <business.santajitpatra@gmail.com>
2023-07-18 17:15:49 +0530

    feat : Insert & Update
    Insert data  to supabase
    Update data fetch from supabase

--- src/components/SmoothieCard.jsx
+++ src/components/SmoothieCard.jsx
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
+
 function SmoothieCard({ smoothie }) {
   return (
     <div className="smoothie_card">
       <h3>{smoothie.title}</h3>
       <p>{smoothie.method}</p>
       <div className="rating">{smoothie.rating}</div>
+      <div className="buttons">
+        <Link to={"/" + smoothie.id}>
+          <span className="material-icons">edit</span>
+        </Link>
+      </div>
     </div>
   );
 }
